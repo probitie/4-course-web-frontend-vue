e2262dac16687c03147092dd3cc9a0f39ac90dc0
probitie <viacheslav.mashkov@gmail.com>
2024-12-08 13:26:26 +0000

    feat: part  1: chat on client side
    * chat works in the app but cannot esablish connection with backend

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <div id="app">
     <h1>Smartphone Store</h1>
     <ProductList />
+    <h2>VueChat</h2>
+    <VueChat />
   </div>
 </template>
 
 <script>
 import ProductList from './components/ProductList.vue';
+import VueChat from './components/VueChat.vue';
 
 export default {
   components: {
     ProductList,
+    VueChat,
   },
 };
 </script>
